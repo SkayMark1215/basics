a6aa7b4db77e1dbc896be4f575802ef4e4070ffc
SkayMark1215 <42638885+SkayMark1215@users.noreply.github.com>
2019-05-24 14:00:13 +0300

    Add files via upload

---
+++ TDEE.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hi! This is calorie calculator." << endl 
+         << "It is based on Mifflin & St. Jeor formula for calculating basal metabolism and then calculate the TDEE." << endl;
+    //colecting data 
+    cout << "Please, enter your weight: ";
+    float weight = 0;
+    cin >> weight;
+    cout << "height: ";
+    float height = 0;
+    cin >> height;
+    cout << "age: ";
+    float age = 0;
+    cin >> age;
+    cout << "gender (m/f): ";
+    char gender = ' ';
+    cin >> gender;
+    cout << "Enter your activity level, where:" << endl
+         << "1.2 - sedentary lifestyle" << endl
+         << "1.375 - average activity (light exercise 1-3 times a week)" << endl
+         << "1.55 - high activity (intensive exercise 3-5 times a week)" << endl
+         << "1.725 - very high activity (heavy physical activity 6-7 times a week)" << endl
+         << "Activity level: ";
+    float activity_level = 0; 
+    cin >> activity_level;    
+    //calculating
+    float metabolism = (10 * weight) + (6.25 * height) - (5 * age);
+    switch (gender)
+    {
+    case 'm':
+        metabolism += 5;
+        cout << "Your basal metabolism: " << metabolism << " kcal." << endl
+             << "Your TDEE: " << metabolism * activity_level << " kcal." << endl;
+        break;
+    case 'f':
+        metabolism -= 161;
+        cout << "Your basal metabolism: " << metabolism << " kcal." << endl
+             << "Your TDEE: " << metabolism * activity_level << " kcal." << endl;
+        break;
+
+    default:
+        cout << "Invalid gender." << endl;
+        break;
+    }
+    cout << "Thank you for using. Bye!"
+
+    system("pause");
+    return 0;
+}
