00dac8466ff183adae6071ed2341486ad2b8e458
SkayMark1215 <42638885+SkayMark1215@users.noreply.github.com>
2019-05-26 14:51:12 +0300

    Update alphabetic_sequence.cpp

--- alphabetic_sequence.cpp
+++ alphabetic_sequence.cpp
@@ -2,55 +2,42 @@
 using namespace std;
 
 int main() {
-    cout << "Please, enter three string values: ";
-    string s1 {'?'};
-    string s2 {'?'};
-    string s3 {'?'};
+    cout << "Please, enter three string to sort them by order: ";
+    string s1, s2, s3;
     cin >> s1 >> s2 >> s3;
-    string fplace {'?'};
-    string splace {'?'};
-    string tplace {'?'};
-    if (s1 >= s2 and s1 >= s3)
-    {
+    //calculating
+    string fplace, splace, tplace;
+    if (s1 >= s2 && s1 >= s3) {
         tplace = s1;
-        if (s2 >= s3)
-        {
+        if (s2 >= s3) {
         splace = s2;
         fplace = s3;
         }
-        else 
-        {
+        else {
         splace = s3;
         fplace = s2;    
         }
     }
-    else
-    {
-        if (s2 >= s1 and s2 >= s3) 
-        {
+    else {
+        if (s2 >= s1 and s2 >= s3) {
             tplace = s2;
-            if (s1 >= s3) 
-            {
+            if (s1 >= s3) {
                 splace = s1;
                 fplace = s3;
             }
-            else
-            {
+            else {
                 splace = s3;
                 fplace = s1;
             }
             
         }
-        else
-        {
+        else {
             tplace = s3;
-            if (s2 >= s1)
-            {
+            if (s2 >= s1) {
                 splace = s2;
                 fplace = s1;
             }
-            else 
-            {
+            else {
                 splace = s1;
                 fplace = s2;
             }
@@ -58,7 +45,7 @@ int main() {
         
     }
     cout << fplace << ", " << splace << ", " << tplace;
-    cin >> s1;  //screen freezing
-
+    
+    system("pause");
     return 0;
 }
