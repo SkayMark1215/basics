4375ea4f992abc61c3996c1573b5e267fc63f6af
SkayMark1215 <42638885+SkayMark1215@users.noreply.github.com>
2019-05-26 14:54:33 +0300

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -5,36 +5,27 @@ int main() {
     cout << "Please, enter an operation and two operands separated by spaces" << endl
          << "Available operations: +, -, *, /" << endl
          << "For example, + 2 13" << endl ;
-    char operation {'?'};
-    double operand1 {0};
-    double operand2 {0};
-    double result {0};
+    char operation;
+    double operand1, operand2, result {0};
     cin >> operation >> operand1 >> operand2;
-    if (operation == '+') 
-    {
+    //calculating
+    if (operation == '+') {
         result = operand1 + operand2;
     }
-    else
-    {
-        if (operation == '-') 
-        {
+    else {
+        if (operation == '-') {
             result = operand1 - operand2;
         }
-        else
-        {
-            if (operation == '*')
-            {
+        else {
+            if (operation == '*') {
                 result = operand1 * operand2;
             }
-            else
-            {
-                if (operation == '/')
-                {
+            else {
+                if (operation == '/') {
                     result = operand1 / operand2;
                 }
-                else
-                {
-                    cout << "Please, check input condition";
+                else {
+                    cout << "Please, check input condition" << endl;
                 }
                 
             }
@@ -42,8 +33,8 @@ int main() {
         }
         
     }
-    cout << "Result is " << result;
-    cin >> operation;   //screen freezing
-
+    cout << "Result is " << result << endl;
+    
+    system("pause");
     return 0;
 }
