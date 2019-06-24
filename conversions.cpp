314591d80afae0d534d68d941e31ddfc04467617
SkayMark1215 <42638885+SkayMark1215@users.noreply.github.com>
2019-06-24 23:23:11 +0300

    Update conversions.cpp

--- conversions.cpp
+++ conversions.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main()
 {
-	double d = 0;
+	double d {0};
 	while (cin >> d) {
 		int i = d;
 		char c = i;
 		int i2 = c;
 		cout << "d==" << d
-			 << " double to int ==" << i
-			 << " char to int==" << i2
-			 << " char(" << c << ")\n";
+		     << " double to int ==" << i
+	             << " char to int==" << i2
+		     << " char(" << c << ")\n";
 	}
-	cin >> d;	//screen freezing
 	
+    system("pause");	
     return 0;
 }
 
