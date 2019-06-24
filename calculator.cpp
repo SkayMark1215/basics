749801262b55ebf5a25a33c0aee296b8efe65cd9
SkayMark1215 <42638885+SkayMark1215@users.noreply.github.com>
2019-06-24 23:17:50 +0300

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -4,34 +4,27 @@ using namespace std;
 int main() {
     cout << "Please, enter an operation and two operands separated by spaces" << endl
          << "Available operations: +, -, *, /" << endl
-         << "For example, + 2 13" << endl ;
+         << "For example, + 2 13" << endl;
     char operation;
     double operand1, operand2, result {0};
     cin >> operation >> operand1 >> operand2;
     //calculating
-    if (operation == '+') {
+    if (operation == '+')
         result = operand1 + operand2;
-    }
     else {
-        if (operation == '-') {
+        if (operation == '-')
             result = operand1 - operand2;
-        }
         else {
-            if (operation == '*') {
+            if (operation == '*')
                 result = operand1 * operand2;
-            }
             else {
-                if (operation == '/') {
+                if (operation == '/')
                     result = operand1 / operand2;
-                }
                 else {
                     cout << "Please, check input condition" << endl;
                 }
-                
             }
-            
         }
-        
     }
     cout << "Result is " << result << endl;
     
