802b04905c4c80f151f1c5dd8ce60f0ed5f9f2c5
SkayMark1215 <42638885+SkayMark1215@users.noreply.github.com>
2019-06-24 23:10:52 +0300

    Update alphabet_table.cpp

--- alphabet_table.cpp
+++ alphabet_table.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() { 
     char letter {'a'};
-    int values = 97;
-    while(values != 123){   //table of chars with their values
+    int values {97};
+    while (values != 123) {   //table of chars with their values
         cout << letter << '\t' << values << endl;
         ++values;
         letter = values;
