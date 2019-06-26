7407d299798311ece0229be9bdfd78555b154045
SkayMark1215 <42638885+SkayMark1215@users.noreply.github.com>
2019-06-26 17:45:11 +0300

    Update into_digits.cpp

--- into_digits.cpp
+++ into_digits.cpp
@@ -3,41 +3,28 @@ using namespace std;
 
 int main() {
     cout << "Please, enter one spelled-out number from one to four: ";
-    string SOnum {" "};
+    string SOnum;
     cin >> SOnum;
     if (SOnum == "One" or SOnum == "one")
-    {
         cout << SOnum << " digitally - 1";
-    }
     else
     {
         if (SOnum == "Two" or SOnum == "two")
-        {
             cout << SOnum << " digitally - 2";
-        }
         else
         {
             if (SOnum == "Three" or SOnum == "three")
-            {
                 cout << SOnum << " digitally - 3";
-            }
             else
             {
                 if (SOnum == "Four" or SOnum == "four")
-                {
                     cout << SOnum << " digitally - 4";
-                }
                 else
-                {
                     cout << "Please, check the input condition";
-                }
-            
             }
-        
         }
-        
     }
-    cin >> SOnum;   //screen freezing
-
+    
+    system("pause");
     return 0;
 }
