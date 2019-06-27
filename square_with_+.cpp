647cea748ce945c052a00b4fafd4d9992f84803f
SkayMark1215 <42638885+SkayMark1215@users.noreply.github.com>
2019-06-27 12:58:41 +0300

    Update square_with_+.cpp

--- square_with_+.cpp
+++ square_with_+.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int i;
 int i2;
-unsigned int number = 0;
+unsigned int number;
+
 void square_plus(int) {
     number = number + i2;
 }
+
 int main() {
     cout << "Please, enter the positive number to square it with + operation: ";
     cin >> number;
     i2 = number;
-    for (int i=1; i<i2; ++i) square_plus(i);
-    cout << number << endl;
-
-    cin >> number; //screen freezing
-
+    for (int i = 1 ; i < i2; i++) square_plus(i);
+    cout << "Result of square is " << number << endl;
+    
+    system("pause");
     return 0;
 }
