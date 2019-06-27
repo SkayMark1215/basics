37005fb5113f56e35a63741c630be7d9a0244f9c
SkayMark1215 <42638885+SkayMark1215@users.noreply.github.com>
2019-06-27 12:43:19 +0300

    Update letter_to_friend.cpp

--- letter_to_friend.cpp
+++ letter_to_friend.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main () {
     cout << "Please, enter your name: ";
-    string your_name {" "};
+    string your_name;
     cin >> your_name;
     cout << "Enter the name of the person you want to write to: ";
-    string name {" "};
+    string name;
     cin >> name;
     cout << "Enter name of your other friend: ";
-    string friend_name {" "};
+    string friend_name;
     cin >> friend_name;
     cout << "Enter 'm' if the friend is male and an 'f' if the friend is female: ";
-    char friend_sex {'?'};
+    char friend_sex;
     cin >> friend_sex;
     cout << "Please, enter the age of recipient: ";
-    int age {-1};
+    int age;
     cin >> age;
     if (age <= 0 or age >= 110)
         cout << "You're kidding!" << endl; 
@@ -25,14 +25,14 @@ int main () {
         cout << "Next year you will be able to vote" << endl;   
     if (age >= 70)
         cout << "I hope you are enjoying retiriment" << endl;         
-    cout << "Dear " << name << ", how are you? Is all OK? Waiting for ur answer..." << endl;
-    cout << "Have u seen " << friend_name << " lately?" << endl; 
+    cout << "Dear " << name << ", how are you? Is all OK? Waiting for ur answer..." << endl
+         << "Have u seen " << friend_name << " lately?" << endl; 
     if (friend_sex=='m') 
         cout << "If you see " << friend_name << " please ask him to call me." << endl << endl;
     if (friend_sex=='f') 
         cout << "If you see " << friend_name << " please ask her to call me." << endl << endl;
     cout << "Your sincerely, " << endl << endl << endl << your_name;
-    cin >> name;    //screen freezing
-
+    
+    system("pause");
     return 0;
 }
