25ea51b935c9421dffe212a3bfb2a55164dc20c3
SkayMark1215 <42638885+SkayMark1215@users.noreply.github.com>
2019-06-27 12:51:00 +0300

    Update odd_or_even.cpp

--- odd_or_even.cpp
+++ odd_or_even.cpp
@@ -3,20 +3,16 @@ using namespace std;
 
 int main() {
     cout << "Please, enter an integer number: ";
-    int number {0};
+    int number;
     cin >> number;
     int determinant;
     determinant = number % 2;
-    /* cout << determinant << endl; Check the determinant output */
+    //cout << determinant << endl; Check the determinant output
     if (determinant = 0)
-    {
         cout << "The number " << number << " is an even number.";
-    }
     else
-    {
         cout << "The number " << number << " is an odd number.";
-    }
-    cin >> number;  //screen freezing
-
+    
+    systme("pause");
     return 0;
 }
