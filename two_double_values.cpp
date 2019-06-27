8933a3f259b92100cf613b428569ba6b67951462
SkayMark1215 <42638885+SkayMark1215@users.noreply.github.com>
2019-06-27 13:00:40 +0300

    Update two_double_values.cpp

--- two_double_values.cpp
+++ two_double_values.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main() {
     cout << "Please, enter two floating-point values: ";
-    double val1 {0};
-    double val2 {0};
+    double val1;
+    double val2;
     cin >> val1 >> val2;
     if (val1 > val2)
         cout << val1 << " > " << val2 << endl;
     if (val1 < val2)
         cout << val2 << " > " << val1 << endl;
-    cout << "The sum of " << val1 << " and " << val2 << " is " << val1 + val2 << endl;       
-    cout << "The difference between " << val1 << " and " << val2 << " is " << val1 - val2 << endl;
-    cout << "The product of " << val1 << " and " << val2 << " is " << val1 * val2 << endl;
-    cout << "The ratio of " << val1 << " and " << val2 << " is " << val1 / val2 << endl;
-    cin >> val1;    //screen freezing
+    cout << "The sum of " << val1 << " and " << val2 << " is " << val1 + val2 << endl
+         << "The difference between " << val1 << " and " << val2 << " is " << val1 - val2 << endl
+         << "The product of " << val1 << " and " << val2 << " is " << val1 * val2 << endl
+         << "The ratio of " << val1 << " and " << val2 << " is " << val1 / val2 << endl;
 
+    system("pause");
     return 0;
 }
